6c78bc81bc1e11fc7af4713d6e9c46374aefada9
dhiauddin@gmail.com <dhiauddin@gmail.com@ab01be70-03df-bc02-5058-5cd044700267>
2012-10-24 07:07:47 +0000

    - put jquery in a folder

---
+++ application/config/form_validation.php
@@ -0,0 +1,24 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+##################################################################################################
+
+//form validation through controller
+//format
+/*
+$config = array	( 
+					'controller/function' => array
+					( 
+						array
+							(
+								'field' => 'login',
+								'label' => 'Login',
+								'rules' => 'trim|required|min_length[6]|max_length[12]|xss_clean'
+							)
+					)
+				);
+*/
+##################################################################################################
+
+
+
+/* End of file form_validator.php */
+/* Location: ./application/config/form_validator.php */
