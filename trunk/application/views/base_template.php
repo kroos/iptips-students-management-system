57bfa8a5513445292441b17bf63f1e6c78b4f4a0
dhiauddin@gmail.com <dhiauddin@gmail.com@ab01be70-03df-bc02-5058-5cd044700267>
2012-10-25 04:13:10 +0000

    - fix all jquery issues

--- trunk/application/views/base_template.php
+++ trunk/application/views/base_template.php
@@ -7,6 +7,7 @@
 		<meta name="keywords" content="Students Management, system, students management system, iptips, islam" />
 		<meta name="author" content="Zaugola" />
 		<link rel="shortcut icon" href="<?=base_url()?>images/favicon.ico" type="image/x-icon" />
+		<link href="<?=base_url()?>css/jquery/jquery-ui-1.9.0.custom.css" rel="stylesheet">
 	<? endblock() ?>
 
 	<? startblock('top_nav') ?>
@@ -61,7 +62,7 @@
 
 	<? startblock('content') ?>
 		<h1>Welcome to the CSS3_four template</h1>
-		<p>This simple, fixed width website template is released under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>. This means you are free to download and use it for personal and commercial projects. However, you <strong>must leave the 'design from css3templates.co.uk' link in the footer of the template</strong>.</p>
+		<p>This simple, fixed width website template is released under a <div class="demo"><a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a></div>. This means you are free to download and use it for personal and commercial projects. However, you <strong>must leave the 'design from css3templates.co.uk' link in the footer of the template</strong>.</p>
 		<p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS3</strong>. You can view more free CSS3 web templates <a href="http://www.css3templates.co.uk">here</a>. This template is a fully documented 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design. There is also a working PHP contact form on the contact page.</p>
 		<h2>Browser Compatibility</h2>
 		<p>This template has been tested in the following browsers:</p>
@@ -71,6 +72,25 @@
 			<li>FireFox 10</li>
 			<li>Google Chrome 17</li>
 			<li>Safari 4</li>
+	<? endblock() ?>
+
+	<? startblock('jscript') ?>
+		<script src="<?=base_url()?>js1/jquery/jquery-1.8.2.js"></script>
+		<script src="<?=base_url()?>js/jquery/jquery-ui-1.9.0.custom.js"></script>
+		<script>
+			$(function() {
+				$( "input[type=submit], a, button", ".demo" )
+					.button();
+				$( "#radioset" ).buttonset();
+			});
+		</script>
+		<script type="text/javascript" src="<?=base_url()?>js/jquery/jquery-ui-timepicker-addon.js"></script>
+		<script type="text/javascript">
+			$(function(){
+				// Datepicker
+				$('#datepicker1').datetimepicker({dateFormat: "yy-mm-dd", timeFormat: "hh:mm:ss", showSecond: true, showMillisec: false, ampm: false, stepHour: 1, stepMinute: 1, stepSecond: 5});
+			});
+		</script>
 	<? endblock() ?>
 
 <? end_extend() ?>
